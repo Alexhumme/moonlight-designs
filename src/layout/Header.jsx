68964992a8d2cf3e60dfae89432aec79661581cb
Alex Valdelamar <93412660+Alexhumme@users.noreply.github.com>
2025-06-04 11:11:01 -0500

    move public resources to src/assets and test the navbar component

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,21 @@
+import logo from "../assets/elements/logo_white.png";
 import React from "react";
+import Navbar from "./Navbar";
 
 export default function Header(params) {
     return (
         <header className="app-header">
             <div className="app-header-left">
-                sdf
+                <a href="/" className="app-logo">
+                    <img 
+                    src={logo}
+                    alt="Logo" 
+                    className="app-logo-image"
+                     />
+                </a>
             </div>
 
-            <nav className="app-header-nav">
-                sdfsd
-            </nav>
+             <Navbar />
 
             <div className="app-header-right">
                 <button type="button" className="login-button">Conoce mas</button>
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar () {
+    return (
+        <nav className="app-header-nav">
+            <ul>
+                <li>Inicio</li>
+                <li>Nosotros</li>
+                <li>Sevicios</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    );
+}
